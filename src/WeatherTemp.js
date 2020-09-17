1215877cde1f22a23229cf155fb3c64742243d0a
Mayatot <66882605+Mayatot@users.noreply.github.com>
2020-09-16 20:36:22 -0400

    woking on converting c to f

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
-export default function WeatherTemp(props) {
-  return (
-    <div className="WeatherTemp">
-          <h1> {Math.round(props.celsius)}</h1>
-      <small className="units">
-        <a href="/">°C</a> | <a href="/">°F</a>
-      </small>
-    </div>
-  );
+export default function WeatherTemp(props) { 
+    const [unit, setUnit] = useState("celsius");
+
+    function showFar(event){
+        event.preventDefault();
+        setUnit("fahrenheit")
+    }
+
+function showCelsius(event){
+  event.preventDefault();
+  setUnit("celsius")
+}
+
+if (unit === "celsius"){
+return (
+  <div className="WeatherTemp">
+    <h1> {Math.round(props.celsius)}</h1>
+    <small className="units">
+      
+        °C
+     {" "}
+      | <a href="/" onClick ={showFar}>°F</a>
+    </small>
+  </div>
+);
+}else {
+  let far = (props.celius * 9/5) + 32;
+ return (
+   <div className="WeatherTemp">
+     <h1> {Math.round(far)}</h1>
+     <small className="units">
+       <a href="/" onClick={showCelsius}>
+         °C
+       </a>{" "}
+       |{" "}
+        °F
+       
+     </small>
+   </div>
+ );
+}
+  
 }
