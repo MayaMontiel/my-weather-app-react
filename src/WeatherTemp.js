d24bcdfd54a88e712ffa8329258d734c6eb5ec47
Mayatot <66882605+Mayatot@users.noreply.github.com>
2020-09-16 21:33:25 -0400

    edited c/f link

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,45 +1,12 @@
-import React, {useState} from "react";
+import React from "react";
 
-export default function WeatherTemp(props) { 
-    const [unit, setUnit] = useState("celsius");
-
-    function showFar(event){
-        event.preventDefault();
-        setUnit("fahrenheit")
-    }
-
-function showCelsius(event){
-  event.preventDefault();
-  setUnit("celsius")
-}
-
-if (unit === "celsius"){
-return (
-  <div className="WeatherTemp">
-    <h1> {Math.round(props.celsius)}</h1>
-    <small className="units">
-      
-        °C
-     {" "}
-      | <a href="/" onClick ={showFar}>°F</a>
-    </small>
-  </div>
-);
-}else {
-  let far = (props.celius * 9/5) + 32;
- return (
-   <div className="WeatherTemp">
-     <h1> {Math.round(far)}</h1>
-     <small className="units">
-       <a href="/" onClick={showCelsius}>
-         °C
-       </a>{" "}
-       |{" "}
-        °F
-       
-     </small>
-   </div>
- );
-}
-  
+export default function WeatherTemp(props) {
+  return (
+    <div className="WeatherTemp">
+      <h1> {Math.round(props.celsius)}</h1>
+      <span className="units">
+        <a href="/">°C </a> |<a href="/">°F </a>
+      </span>
+    </div>
+  );
 }
